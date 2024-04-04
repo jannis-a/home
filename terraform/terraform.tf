7e5100565e3f007f786ab430b799c330890a2125
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:28:29 +0200

    feat(terraform): add space remote backend config

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,15 @@
 terraform {
   required_version = ">= 1.6.2"
 
+  backend "remote" {
+    hostname     = "spacelift.io"
+    organization = "jannis-a"
+
+    workspaces {
+      name = "home"
+    }
+  }
+
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
