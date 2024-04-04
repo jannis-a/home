5daccc1bf8970275aa24d15643bae3dafc16cc90
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:57:53 +0200

    fix(github): blank merge commit message

--- terraform/github.tf
+++ terraform/github.tf
@@ -15,5 +15,5 @@ resource "github_repository" "this" {
 
   delete_branch_on_merge      = true
   squash_merge_commit_title   = "PR_TITLE"
-  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_message = "BLANK"
 }
