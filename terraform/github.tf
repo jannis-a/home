01523f79917279795c1ec7820222f6072067b9b4
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:36:37 +0200

    feat(github): change squash merge commit details

--- terraform/github.tf
+++ terraform/github.tf
@@ -6,5 +6,7 @@ resource "github_repository" "this" {
   allow_merge_commit = false
   allow_rebase_merge = false
 
-  delete_branch_on_merge = true
+  delete_branch_on_merge      = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "COMMIT_MESSAGES"
 }
