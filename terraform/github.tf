61b6a2acf8c92a1ce759a321da465a7821f89c6b
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:32:32 +0200

    feat(github): add repo visibility

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,9 +1,10 @@
 resource "github_repository" "this" {
-  name = "home"
+  name       = "home"
+  visibility = "private"
 
-  allow_squash_merge     = true
-  allow_merge_commit     = true
-  allow_rebase_merge     = false
+  allow_squash_merge = true
+  allow_merge_commit = true
+  allow_rebase_merge = false
+
   delete_branch_on_merge = true
-
 }
