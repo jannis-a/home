7176fbb63e1e7dd3d8795a2b5ab8a29fc8302fbe
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:19:18 +0200

    feat(github): auto delete branch

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,7 +1,9 @@
 resource "github_repository" "this" {
   name = "home"
 
-  allow_squash_merge = true
-  allow_merge_commit = false
-  allow_rebase_merge = false
+  allow_squash_merge     = true
+  allow_merge_commit     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+
 }
