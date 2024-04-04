915843404d0cc34d6478f413f709ce20c14fbe32
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 23:10:56 +0200

    feat(github): allow auto-merge

--- terraform/github.tf
+++ terraform/github.tf
@@ -11,6 +11,7 @@ resource "github_repository" "this" {
   allow_squash_merge  = true
   allow_merge_commit  = false
   allow_rebase_merge  = false
+  allow_auto_merge    = true
   allow_update_branch = true
 
   delete_branch_on_merge      = true
