14dfc867eff2fae7c665af1015e41e4b0e058627
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:35:12 +0200

    fix(github): disallow merge commit

--- terraform/github.tf
+++ terraform/github.tf
@@ -3,7 +3,7 @@ resource "github_repository" "this" {
   visibility = "private"
 
   allow_squash_merge = true
-  allow_merge_commit = true
+  allow_merge_commit = false
   allow_rebase_merge = false
 
   delete_branch_on_merge = true
