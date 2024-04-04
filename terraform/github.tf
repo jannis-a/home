f61acdcb701207bdbc2e371952740d695ac64c7f
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2024-04-04 09:38:46 +0200

    feat(github): suggest updating from main

--- terraform/github.tf
+++ terraform/github.tf
@@ -8,9 +8,10 @@ resource "github_repository" "this" {
   has_projects    = false
   has_wiki        = false
 
-  allow_squash_merge = true
-  allow_merge_commit = false
-  allow_rebase_merge = false
+  allow_squash_merge  = true
+  allow_merge_commit  = false
+  allow_rebase_merge  = false
+  allow_update_branch = true
 
   delete_branch_on_merge      = true
   squash_merge_commit_title   = "PR_TITLE"
