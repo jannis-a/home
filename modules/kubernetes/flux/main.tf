e058527b324cc1f3f996f9811b3285bc7e2b6bf1
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2025-09-23 19:41:22 +0200

    feat(terraform): add flux module

---
+++ modules/kubernetes/flux/main.tf
@@ -0,0 +1,21 @@
+data "github_repository" "this" {
+  name = var.repository
+}
+
+resource "tls_private_key" "this" {
+  algorithm = "ED25519"
+}
+
+resource "github_repository_deploy_key" "this" {
+  title      = var.key_name
+  repository = data.github_repository.this.name
+  key        = tls_private_key.this.public_key_openssh
+  read_only  = false
+}
+
+resource "flux_bootstrap_git" "this" {
+  depends_on = [github_repository_deploy_key.this]
+
+  path = var.path
+
+}
