7fbe847b325e0b2a7c36a5aa91ba2c1ecbb0af01
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2025-09-25 07:36:19 +0200

    feat(infra): add coredns flux release

--- modules/kubernetes/coredns/main.tf
+++ modules/kubernetes/coredns/main.tf
@@ -1,31 +1,23 @@
-resource "helm_release" "this" {
-  repository = "oci://ghcr.io/coredns/charts"
+data "helm_template" "this" {
+  repository = "https://coredns.github.io/helm"
   chart      = "coredns"
   version    = "1.43.3"
-  namespace  = "kube-system"
-  name       = "coredns"
+  namespace  = var.namespace
+  name       = var.name
+}
 
-  set = [
-    {
-      name  = "replicaCount"
-      value = 1
-    },
-    {
-      name  = "serviceAccount.create"
-      value = true
-    },
-    {
-      name  = "service.clusterIP"
-      value = var.service_ips[0]
-    },
-    {
-      name  = "service.ipFamilyPolicy"
-      value = "RequireDualStack"
-    },
-  ]
+resource "local_file" "flux" {
+  for_each = fileset("${path.module}/flux", "*.yaml")
 
-  set_list = [{
-    name  = "service.clusterIPs"
-    value = var.service_ips
-  }]
+  filename             = "${var.deploy_path}/${var.name}/${each.key}"
+  directory_permission = "0755"
+  file_permission      = "0644"
+  content = templatefile("flux/${each.key}", {
+    chart_repository = data.helm_template.this.repository
+    chart_name       = data.helm_template.this.chart
+    chart_version    = data.helm_template.this.version
+    name             = var.name
+    namespace        = var.namespace
+    service_ips      = var.service_ips
+  })
 }
--- modules/kubernetes/coredns/variables.tf
+++ modules/kubernetes/coredns/variables.tf
@@ -1,3 +1,17 @@
+variable "deploy_path" {
+  type = string
+}
+
+variable "name" {
+  type    = string
+  default = "coredns"
+}
+
+variable "namespace" {
+  type    = string
+  default = "kube-system"
+}
+
 variable "service_ips" {
   type = list(string)
 }
